b8522e2675360209f0754e25d1482a113d99c2ed
marielaure1 <edjour.marielaure@gmail.com>
2022-07-03 17:51:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(".nav-item").each((e, item)=>{
+    $(item).mouseenter(()=>{
+        position = $(item).position()
+        width = $(item).width() + 16
+        $(".animate-backdrop").css({top: position.top, left: position.left, visibility: "visible", opacity: 1, width: width + 4})
+    })
+})
+
+$("nav").mouseleave(()=>{
+    $(".animate-backdrop").css({visibility: "hidden", opacity: 0})
+})
+
+
+// $("main").scroll(()=>{
+//     $(".models").each((e, element)=>{
+//         $(".content").eq(e).removeClass("active")
+//         let offset = $(element).offset()
+//         if(offset.top == $("main").scrollTop()){
+//             $(".content").eq(e).addClass("active")
+//         }
+//     })
+// })
